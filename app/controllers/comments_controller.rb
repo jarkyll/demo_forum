b4bf982675c161710529a4251a7a2a6fe8e8fd2c
Nabeel Virani <nabeelvirani@gmail.com>
2015-10-25 21:36:41 -0500

    added a create and edit page for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,10 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment].permit(:comment))
+    @comment = @post.comments.create(comment_params)
+    @comment.user_id = current_user.id if current_user
+    @comment.save
+    # you need to save the comment
     #you get the post and then create a comment allowing a comment
     if @comment.save
       redirect_to post_path(@post)
@@ -11,7 +14,36 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
 
 
+  def update
+    #so if we have clicked the button to update,
+    #then update else you are still editing
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+  end
 
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
+
+  
 end
